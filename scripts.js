2b4548899946fdf8a080c8a6cb5de620d0228790
Parthiv Saikia <parthivsaikia0226@gmail.com>
2025-07-09 21:43:26 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,101 @@
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+/**
+* Utility function to update the button text and aria-label.
+*/
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.innerText = newCta;
+}
+
+/**
+* Utility function to update the theme setting on the html tag
+*/
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+/**
+* On page load:
+*/
+
+/**
+* 1. Grab what we need from the DOM and system settings on page load
+*/
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+* 2. Work out the current site settings
+*/
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+/**
+* 3. Update the theme setting and button text accoridng to current settings
+*/
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+* 4. Add an event listener to toggle the theme
+*/
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+});
+
+
+const tickSound = document.getElementById('tick-sound');
+let isSoundAllowed = false;
+
+// Wait for user interaction to allow audio
+document.addEventListener("click", () => {
+    isSoundAllowed = true;
+});
+
+
+let hr=document.getElementById('hr');
+let min=document.getElementById('min');
+let sec=document.getElementById('sec');
+
+function displayTime(){
+    let date=new Date();
+    let hh=date.getHours();
+    let mm=date.getMinutes();
+    let ss=date.getSeconds();
+
+    let hRotation=30*hh+mm/2;
+    let mRotation=6*mm;
+    let sRotation=6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+    if (isSoundAllowed) {
+    tickSound.currentTime = 0;
+    tickSound.play().catch(() => {}); // prevent browser errors if any
+    }
+    
+}
+
+setInterval(displayTime, 1000);
