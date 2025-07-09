0bc2ab66f12a01dbe5f29c8eae623966f78edf92
Parthiv_Saikia <parthivsaikia0226@gmail.com>
2025-07-09 21:51:00 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,8 +15,6 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
 */
 function updateButton({ buttonEl, isDark }) {
   const newCta = isDark ? "Change to light theme" : "Change to dark theme";
-  // use an aria-label if you are omitting text on the button
-  // and using a sun/moon icon, for example
   buttonEl.setAttribute("aria-label", newCta);
   buttonEl.innerText = newCta;
 }
@@ -29,31 +27,19 @@ function updateThemeOnHtmlEl({ theme }) {
 }
 
 
-/**
-* On page load:
-*/
 
-/**
-* 1. Grab what we need from the DOM and system settings on page load
-*/
 const button = document.querySelector("[data-theme-toggle]");
 const localStorageTheme = localStorage.getItem("theme");
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-/**
-* 2. Work out the current site settings
-*/
+
 let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 
-/**
-* 3. Update the theme setting and button text accoridng to current settings
-*/
+
 updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
 updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
-/**
-* 4. Add an event listener to toggle the theme
-*/
+
 button.addEventListener("click", (event) => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
@@ -68,7 +54,7 @@ button.addEventListener("click", (event) => {
 const tickSound = document.getElementById('tick-sound');
 let isSoundAllowed = false;
 
-// Wait for user interaction to allow audio
+// Wait for user interaction to allow audio because chrome and modern browsers block automatic sound playing
 document.addEventListener("click", () => {
     isSoundAllowed = true;
 });
